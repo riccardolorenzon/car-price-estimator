3b904600df3a6c1dbf6afc33480ed6c03dc67f19
Riccardo Lorenzon <riccardo.lorenzon@gmail.com>
2022-12-01 17:02:24 +0100

    E2E test, reports DTO.

---
+++ src/reports/dtos/create-report-dto.ts
@@ -0,0 +1,25 @@
+import { isNumber, IsNumber, IsString, Max, Min } from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  lng: number;
+
+  lat: number;
+
+  price: number;
+}
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'asamlkdasm112@aasasa.com';
+    const email = 'asamlkdasm11222@aasasa.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({
@@ -29,5 +29,23 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
