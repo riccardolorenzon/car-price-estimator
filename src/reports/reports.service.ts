161f7cda76637148b1a7211dc83463be0672399a
Riccardo Lorenzon <riccardo.lorenzon@gmail.com>
2022-12-08 11:29:34 +0100

    fix: select fields and approve condition

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -13,12 +13,13 @@ export class ReportsService {
   createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
     return this.repo
       .createQueryBuilder()
-      .select('*')
+      .select('AVG(price)', 'price')
       .where('make = "make', { make })
       .andWhere('model = :model', { model })
       .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
       .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
       .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
       .orderBy('mileage - :mileage', 'DESC')
       .setParameters({ mileage })
       .getRawMany();
